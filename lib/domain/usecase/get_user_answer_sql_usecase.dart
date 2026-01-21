c45327de81eea9f1525f08c948d521e64df41979
sedraal <sssedraalllkateb@gmail.com>
2026-01-21 09:57:53 +0200

    انشاء  DIALOG CONFERM  لعملية المزامنة  اضافة ال  async bloc  الى  depandese injection  تعديل الخوارزمية الخاصة بالاسئلة  بسبب تعدد انواع الاسئلة متل  text , dropdown , checbox , est  بدل ان اعمل لكل نوع wedgit  عملت نوعين من ال widget  :   - الاسئلة ذات الاجاب الواحدة  , 2- اسئلة ذات الاجابات المتعدد  حذف كل انواع الاسئلة واعتماد الخوارزمية الجديدة  اضافة ال event الى الواجهات الرئيسية  تجربة ال api  الخاصة بالاوفلاين وحل مشاكلها   اضافة synchronizeUsersAnswers لرفع المستخدم والاجوبة الخاصة فيه ك usecase تجرييب ال  sync 
     

---
+++ lib/domain/usecase/get_user_answer_sql_usecase.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:formify/data/network/failure.dart';
+import 'package:formify/domain/models/request.dart';
+import 'package:formify/domain/repostitory/repository_sql.dart';
+
+class GetUserAnswerSqlUsecase extends Equatable {
+  final RepositorySql _repository;
+  const GetUserAnswerSqlUsecase(this._repository);
+  Future<Either<Failure, List<UserRequest>>> execute() async {
+    return await _repository.getDataSql();
+  }
+
+  @override
+  List<Object?> get props => [_repository];
+}
