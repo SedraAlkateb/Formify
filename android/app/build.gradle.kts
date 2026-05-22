fba77e31fa10eca11b9d601b570abd63ccdc8467
Sedra Alkhateeb <sssedraalllkateb@gmail.com>
2026-05-22 09:29:10 +0300

    animation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     namespace = "com.example.formify"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-
+    //buildToolsVersion="35.0.0"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
